76ce8757728cd0e932ca58b356e3462e9a5c03c2
鈴なりれの <115887976+Re-No-Da@users.noreply.github.com>
2023-12-07 13:55:15 +0000

    main

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # template-discord.js-v14
+
+１．`npm init -y`
+
+２．`npm i discord.js dotenv` ( i = install )
+
+３．Edit .env
+```
+TOKEN=HERE
+clientId=HERE
+```
+
+４．`node index.js`
---
+++ src/index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const {
+    Client,
+    GatewayIntentBits,
+    Events,
+    EmbedBuilder
+} = require('discord.js');
+
+const client = new Client({
+    intents:
+        [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMessages,
+        ]
+});
+
+client.once(Events.ClientReady, (c) => {
+    console.log(`Log in with ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async message => {
+    if (message.author.bot || message.webhookId) return;
+
+    if (message.content == '!hi') {
+        message.reply(`hi! ${message.author}`);
+    }
+
+    if (message.content == '!ping') {
+        message.channel.send('pong!');
+    }
+
+    if (content.startsWith('!me')) {
+        let user;
+        if (message.mentions.users.size > 0) {
+            user = message.mentions.users.first();
+        } else {
+            user = message.author;
+        }
+
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: `Avatar of ${user.username}` })
+            .setImage(user.displayAvatarURL({ dynamic: true, size: 2048 }))
+            .setColor('Green')
+            .setFooter({ text: `Created by ${user.username}` });
+
+        await message.channel.send({ embeds: [embed] });
+    }
+});
+
+client.login(process.env.TOKEN);
